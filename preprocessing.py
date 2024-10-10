9535cddd4e34bb1eaa1b7a38a795f46852d9d35f
Sboof <amaach@student.1337.ma>
2024-10-10 07:20:27 +0100

    modelisation

--- preprocessing.py
+++ preprocessing.py
@@ -3,60 +3,63 @@ import matplotlib.pyplot as plt
 from mne.channels import make_standard_montage
 from mne.datasets import eegbci
 
-def fetch_events(data_filtered, tmin=-1., tmax=4.):
-    event_ids = dict(T1=0, T2=1)
-    events, _ = mne.events_from_annotations(data_filtered, event_id=event_ids)
-    picks = mne.pick_types(data_filtered.info, meg=False, eeg=True, stim=False, eog=False, exclude='bads')
-    epochs = mne.Epochs(data_filtered, events, event_ids, tmin, tmax, proj=True,
-                        picks=picks, baseline=None, preload=True)
-    labels = epochs.events[:, -1]
-    return labels, epochs
+class preprocess:
+    def __init__(self, plot=False):
+        self._dict = dict(T0=0, T1=1, T2=2)
+        self._plot = plot
 
+    def fetch_events(self, data_filtered, tmin=-1., tmax=4.):
+        event_ids = self._dict
+        events, _ = mne.events_from_annotations(data_filtered, event_id=event_ids)
+        picks = mne.pick_types(data_filtered.info, meg=False, eeg=True, stim=False, eog=False, exclude='bads')
+        epochs = mne.Epochs(data_filtered, events, event_ids, tmin, tmax, proj=True,
+                            picks=picks, baseline=None, preload=True)
+        labels = epochs.events[:, -1]
+        return labels, epochs
 
-def filter_data(raw, plot, montage=make_standard_montage('standard_1020')):
-    data_filter = raw.copy()
-    data_filter.set_montage(montage)
-    data_filter.filter(7, 30, fir_design='firwin', skip_by_annotation='edge')
-    if plot:
-        print("plotting after modification")
-        mne.viz.plot_raw(data_filter, scalings={"eeg": 75e-6}, show=False)
-        plt.show()
-    return data_filter
+    def filter_data(self, raw, montage=make_standard_montage('standard_1020')):
+        data_filter = raw.copy()
+        data_filter.set_montage(montage)
+        data_filter.filter(7, 30, fir_design='firwin', skip_by_annotation='edge')
+        if self._plot:
+            print("plotting after modification")
+            mne.viz.plot_raw(data_filter, scalings={"eeg": 75e-6}, show=False)
+            plt.show()
+        return data_filter
 
+    def prepare_data(self, raw, montage=make_standard_montage('standard_1020')):
+        raw.rename_channels(lambda x: x.strip('.'))
+        eegbci.standardize(raw)
+        raw.set_montage(montage)
 
-def prepare_data(raw, plot, montage=make_standard_montage('standard_1020')):
-    raw.rename_channels(lambda x: x.strip('.'))
-    eegbci.standardize(raw)
-    raw.set_montage(montage)
+        # plot
+        if self._plot:
+            montage = raw.get_montage()
+            print("plotting before modification")
+            montage.plot(show=False)
+            mne.viz.plot_raw(raw, scalings={"eeg": 75e-6}, show=False)
+            plt.show()
+        return raw
 
-    # plot
-    if plot:
-        montage = raw.get_montage()
-        print("plotting before modification")
-        montage.plot(show=False)
-        mne.viz.plot_raw(raw, scalings={"eeg": 75e-6}, show=False)
-        plt.show()
-    return raw
+    def fetch_data(self, subject_folder_path, sfreq=None):
+        dataset = []
+        subject = []
+        files = sorted(glob.glob(os.path.join(subject_folder_path, f"{subject_folder_path[-4:]}R[0-9][0-9].edf")))
+        for file in files:
+            if file.endswith(".edf"):
+                subject_data = mne.io.read_raw_edf(os.path.join(subject_folder_path, file), preload=True)
+                if sfreq is None:
+                    sfreq = subject_data.info["sfreq"]
+                if subject_data.info["sfreq"] == sfreq:
+                    subject.append(subject_data)
+                else:
+                    break
+        dataset.append(mne.concatenate_raws(subject))
+        raw = mne.io.concatenate_raws(dataset)
+        return raw
 
-def fetch_data(subject_folder_path, sfreq=None):
-    dataset = []
-    subject = []
-    files = sorted(glob.glob(os.path.join(subject_folder_path, f"{subject_folder_path[-4:]}R[0-9][0-9].edf")))
-    for file in files:
-        if file.endswith(".edf"):
-            subject_data = mne.io.read_raw_edf(os.path.join(subject_folder_path, file), preload=True)
-            if sfreq is None:
-                sfreq = subject_data.info["sfreq"]
-            if subject_data.info["sfreq"] == sfreq:
-                subject.append(subject_data)
-            else:
-                break
-    dataset.append(mne.concatenate_raws(subject))
-    raw = mne.io.concatenate_raws(dataset)
-    return raw
-
-def process(subject_folder_path, plot=False):
-    raw = fetch_data(subject_folder_path=subject_folder_path)
-    raw = prepare_data(raw, plot)
-    fildered_data = filter_data(raw, plot)
-    return fetch_events(fildered_data)
+    def process(self, subject_folder_path):
+        raw = self.fetch_data(subject_folder_path=subject_folder_path)
+        raw = self.prepare_data(raw)
+        fildered_data = self.filter_data(raw)
+        return self.fetch_events(fildered_data)
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from preprocessing import process, glob
+from preprocessing import preprocess, glob
 from mne.decoding import CSP
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score, StratifiedKFold, train_test_split
@@ -6,35 +6,41 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
 from sklearn.multiclass import OneVsRestClassifier
 
+class train:
+    def __init__(self) -> None:
+        csp = CSP(cov_est='epoch')
+        clf = OneVsRestClassifier(LogisticRegression(max_iter=100))
 
-def fit(X_train, y_train, X_test, y_test, pipeline, cv):
-    # Evaluate model performance using cross-validation on the training data
-    scores = cross_val_score(pipeline, X_train, y_train, cv=cv, scoring='accuracy')
-    print(f'Cross-validation accuracy scores: {scores}')
-    print(f'Mean accuracy: {scores.mean()}')
-    pipeline.fit(X_train, y_train)
-    # Validation test
-    y_pred = pipeline.predict(X_test)
-    print(classification_report(y_test, y_pred))
-    return pipeline
+        self._pipeline = Pipeline([
+            ('csp', csp),
+            ('classifier', clf)
+        ])
+        self._cv = StratifiedKFold(n_splits=5)
 
+    def fit(self, X_train, y_train, X_test, y_test):
+        # Evaluate model performance using cross-validation on the training data
+        scores = cross_val_score(self._pipeline, X_train, y_train, cv=self._cv, scoring='accuracy')
+        print(f'Cross-validation accuracy scores: {scores}')
+        print(f'Mean accuracy: {scores.mean()}')
+        self._pipeline.fit(X_train, y_train)
+        # Validation test
+        y_pred = self._pipeline.predict(X_test)
+        print(classification_report(y_test, y_pred))
+
+    @property
+    def pipeline(self):
+        return self._pipeline
 
 def training(plot=False):
-    csp = CSP(cov_est='epoch')
-    clf = OneVsRestClassifier(LogisticRegression(max_iter=100))
-
-    pipeline = Pipeline([
-        ('csp', csp),
-        ('classifier', clf)
-    ])
-    cv = StratifiedKFold(n_splits=5)
+    train_module = train()
 
     subject_folder = "/nfs/sgoinfre/goinfre/Perso/amaach/Downloads/google/eeg-motor-movementimagery-dataset-1.0.0/files/S001"
-    labels, epochs = process(subject_folder, plot)
+    preprocessmodule = preprocess()
+    labels, epochs = preprocessmodule.process(subject_folder, plot)
     if not plot:
         epochs_data_train = epochs.get_data()
         X_train, X_test, y_train, y_test = train_test_split(epochs_data_train, labels, test_size=0.2, random_state=42)
-        model = fit(X_train, y_train, X_test, y_test, pipeline, cv)
+        model = train_module.fit(X_train, y_train, X_test, y_test)
 
 if __name__ == '__main__':
     training(True)
